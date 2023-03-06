e944e154de5b59dd8e76c23425d114ffd2e0a211
Omwano <abekahellena@gmail.com>
2023-03-06 10:56:20 +0300

    update functions

--- namecalc.js
+++ namecalc.js
@@ -1,20 +1,43 @@
-document.querySelector('form').addEventListener('submit' , (Event) =>{
-  Event.preventDefault(); //Prevents the form from submitting.
 
+  //define akan male names
+  const maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+
+  //define akan female names 
+  const femaleNames = [
+    "Akosua",
+    "Adwao",
+    "Abenaa",
+    "Akua",
+    "Ama"
+  ];
+
+  function akanName() {
   //get a user's date of birth
-  let monthBirth = parseInt(document.querySelector('#month').value);
-  let dateBirth = parseInt(document.querySelector('#date').value);
-  let yearBirth = parseInt(document.querySelector('#year').value);
+  const dob = document.getElementById("date");
+  const month = document.getElementById("month");
+  const year = document.getElementById("year");
+  
+  if (dob.value === ""){
+    alert("Please enter date of birth");
+    return
+  }
 
-  //calculate day of week of birth
-  let c = Math.floor(yy / 100);
+    //calculate day of week of birth
   let cc = 2 - (c % 4);
-  let yy = yearBirth % 100;
-  let mm = monthBirth - 1;
-  let dd = dateBirth;
-  let dayOfWeek=( ( (c/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+  let yy = year % 100;
+  let mm = month - 1;
+  let dd = dob.getdob();
+
+  let dayOfWeek = Math.floor( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
 
   let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   alert('Your day of birth is ${daysOfWeek[dayOfWeek]}');
-})
-function akanName()
+}
